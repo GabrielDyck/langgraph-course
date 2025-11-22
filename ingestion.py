f7af04708cf771463cc35a320453e466fc95fc21
GabrielDyck <gabrielfernandodyck@gmail.com>
2025-11-22 13:48:32 -0300

    changes

--- ingestion.py
+++ ingestion.py
@@ -1,5 +1,5 @@
 from dotenv import load_dotenv
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_classic.text_splitter import RecursiveCharacterTextSplitter
 from langchain_chroma import Chroma
 from langchain_community.document_loaders import WebBaseLoader
 from langchain_openai import OpenAIEmbeddings
